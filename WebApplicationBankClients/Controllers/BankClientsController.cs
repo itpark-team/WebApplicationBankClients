17d6fb35ff9e5077fc4d4ce0622f478d28d5ff06
itpark <a.m.smyslov@yandex.ru>
2023-03-09 21:25:23 +0300

    done rounting

--- WebApplicationBankClients/Controllers/BankClientsController.cs
+++ WebApplicationBankClients/Controllers/BankClientsController.cs
@@ -5,35 +5,36 @@ using WebApplicationBankClients.Repository;
 namespace WebApplicationBankClients.Controllers;
 
 [ApiController]
+[Route("[controller]/")]
 public class BankClientsController : ControllerBase
 {
     private BankClientsRepository _bankClientsRepository = BankClientsRepository.Instance;
 
-    [HttpGet("BankClients/{id}")]
+    [HttpGet("{id}")]
     public BankClient GetClientById(int id)
     {
         return _bankClientsRepository.GetById(id);
     }
 
-    [HttpGet("BankClients")]
+    [HttpGet]
     public List<BankClient> GetAllClients()
     {
         return _bankClientsRepository.GetAll();
     }
 
-    [HttpPost("BankClients")]
+    [HttpPost]
     public void AddNewClient(BankClient bankClient)
     {
         _bankClientsRepository.AddNew(bankClient);
     }
 
-    [HttpDelete("BankClients/{id}")]
+    [HttpDelete("{id}")]
     public void DeleteClientById(int id)
     {
         _bankClientsRepository.DeleteById(id);
     }
 
-    [HttpPut("BankClients/{id}")]
+    [HttpPut("{id}")]
     public void UpdateClientById(int id, BankClient bankClient)
     {
         _bankClientsRepository.UpdateClientById(id, bankClient);
